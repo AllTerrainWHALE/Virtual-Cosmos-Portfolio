ffc14ebf4e1bda3f221bee5411ef220e37ac29ef
AllTerrainWHALE <63456304+AllTerrainWHALE@users.noreply.github.com>
2025-09-10 18:44:18 +0100

    Fixed missing commit
    Missed out committing `countAssets()`

--- js/solarsys.js
+++ js/solarsys.js
@@ -444,6 +444,17 @@ function updateFollowedInfo() {
     }
 }
 
+// Count total assets (you'll need to implement this based on your scene)
+function countAssets(settings) {
+    let count = 1; // Count the current object
+    if (settings.satellites) {
+        for (const satellite of Object.values(settings.satellites)) {
+            count += countAssets(satellite);
+        }
+    }
+    return count;
+}
+
 
 
 function createStarField() {
